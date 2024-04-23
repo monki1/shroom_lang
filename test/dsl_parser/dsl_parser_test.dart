dcb42f6d82cc18e2d3c82c975eedadf16aed3fc4
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-23 19:18:09 -0400

    Update dependencies and fix import in dsl_parser_test.dart

--- test/dsl_parser/dsl_parser_test.dart
+++ test/dsl_parser/dsl_parser_test.dart
@@ -1,19 +1,17 @@
 import 'package:petitparser/petitparser.dart';
 import 'package:test/test.dart';
-import 'package:shroom_lang/dsl_parser/dsl_parser.dart'; 
+import 'package:shroom_lang/dsl_parser/dsl_parser.dart';
 
-void main() async{
+void main() async {
   group('DSL Parser Tests', () {
     late Parser parser;
 
     setUp(() {
-
       parser = DSLParserDefinition().build();
     });
 
     test('Parser Initialization', () {
       expect(parser, isNotNull); // Basic test to check if the parser is created
     });
-
   });
 }
