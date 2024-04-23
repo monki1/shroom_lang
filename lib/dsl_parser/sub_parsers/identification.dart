f3627e335fd40663bfa197d0916087109f4f714e
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-23 12:46:48 -0400

    Update path() and integerId() parsers to return a Map with type and value

--- lib/dsl_parser/sub_parsers/identification.dart
+++ lib/dsl_parser/sub_parsers/identification.dart
@@ -2,20 +2,25 @@ import 'package:petitparser/petitparser.dart';
 import 'identifier.dart';
 
 // Define the parser for a path
-Parser path() {
-  return identifier().plusSeparated(char('.'));
+Parser<Map<String, dynamic>> path() {
+  return identifier().plusSeparated(char('.')).map((list) => 
+    {"type": "path", "value": list.elements}
+  );
 }
 
 // Define the parser for an integer identifier
-Parser integerId() {
-  return digit().plus().flatten();
+Parser<Map<String, dynamic>> integerId() {
+  return digit().plus().flatten().map((string) => 
+    {"type": "id", "value": int.parse(string)}
+  );
 }
 
 // Define the parser for identification
-Parser identification() {
-  return (path() | integerId()).flatten();
+Parser<Map<String, dynamic>> identification() {
+  return (path() | integerId()).cast<Map<String, dynamic>>();
 }
 
-Parser key() {
-  return identifier().trim();
+Parser<String> key() {
+  // This parser returns a string, which is just the flattened result of an identifier, trimmed.
+  return identifier().trim().flatten();
 }
