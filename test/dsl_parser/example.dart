cab87bbfd0853005658e5c4cff6bcaf221f55a3a
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-24 09:06:25 -0400

    Refactor DSLParser test to handle complex nested objects and test error handling

--- test/dsl_parser/example.dart
+++ test/dsl_parser/example.dart
@@ -3,27 +3,32 @@
 Map<String, dynamic> EXAMPLES = {
   // Create operations
   "+  id123 profile : {address  :  {city:NewYork state:NY } friends:[@123] }": {
-                'operation': 'create',
-                'path': ['id123'],
-                'attributes': {
-                  'profile': {
-                    'type': 'node',
-                    'value': {
-                      'address': {
-                        'type': 'node',
-                        'value': {
-                          'city': {'type': 'string', 'value': 'NewYork'},
-                          'state': {'type': 'string', 'value': 'NY'}
-                        }
-                      },
-                      'friends': {
-                        'type': 'list',
-                        'value': [{'type': 'node_ref', 'value': {'type': 'id', 'value': 123}}]
-                      }
-                    }
-                  }
-                }
-              },
+    'operation': 'create',
+    'path': ['id123'],
+    'attributes': {
+      'profile': {
+        'type': 'node',
+        'value': {
+          'address': {
+            'type': 'node',
+            'value': {
+              'city': {'type': 'string', 'value': 'NewYork'},
+              'state': {'type': 'string', 'value': 'NY'}
+            }
+          },
+          'friends': {
+            'type': 'list',
+            'value': [
+              {
+                'type': 'node_ref',
+                'value': {'type': 'id', 'value': 123}
+              }
+            ]
+          }
+        }
+      }
+    }
+  },
 
   // // Read operation
   // "@id123:profile": [
