a75b81b8a91a652296b8f466a852483ac51e80e1
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-24 09:05:26 -0400

    Add ShroomLang class with parse() method

--- lib/shroom_lang.dart
+++ lib/shroom_lang.dart
@@ -1,1 +1,20 @@
+import 'package:petitparser/petitparser.dart';
 
+import 'dsl_parser/dsl_parser.dart';
+
+class ShroomLang {
+  static final parser = DSLParserDefinition().build();
+
+  static dynamic parse(String input) {
+    Result? value;
+    try {
+      value = parser.parse(input);
+      return value is Success ? value.value : null;
+    } catch (e) {
+      print('Parsing failed: $input');
+      print('Error: $e');
+      print('Error: ${value?.message} at position ${value?.position}');
+    }
+    return null;
+  }
+}
