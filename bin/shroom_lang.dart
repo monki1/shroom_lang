e920c8fc6d9e79b63f4b46f56a578b6022efbd2d
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-25 23:37:37 -0400

    Comment out main function in shroom_lang.dart

--- bin/shroom_lang.dart
+++ bin/shroom_lang.dart
@@ -1,5 +1,5 @@
-import 'package:shroom_lang/shroom_lang.dart' as shroom_lang;
+// import 'package:shroom_lang/shroom_lang.dart' as shroom_lang;
 
-void main(List<String> arguments) {
-  print('bin');
-}
+// void main(List<String> arguments) {
+//   print('bin');
+// }
